24be9f0da7a4cb71aae14570b1d859c50f1b3f21
Yokesh <yokeshyokesh1131.sse@saveetha.com>
2026-04-06 13:38:56 +0530

    added size chart and used polymorphism

--- Amazon/data/products.js
+++ Amazon/data/products.js
@@ -29,24 +29,26 @@ class Product {
     getPrice(){
         return `${this.price}`;
     }
+    extraInfoHTML() {
+        return '';
+    }
 }
-const product1 = new Product({
-        id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-        image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-        name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-        rating: {
-        stars: 45,
-        count: 87
-        },
-        price: 1090,
-        keywords: [
-        "socks",
-        "sports",
-        "apparel"
-        ]
-    });
-console.log(product1);
 
+class clothing extends Product {
+    sizeChartLink;
+
+    constructor(productDetails) {
+        super(productDetails);
+        this.sizeChartLink = productDetails.sizeChartLink;
+    }
+    extraInfoHTML() {
+        return `
+        <a href="${this.sizeChartLink}" target="_blank">
+            Size Chart 
+        </a>
+        `;
+    }
+}
 
 export const products = [
     {
@@ -152,7 +154,9 @@ export const products = [
         "hoodies",
         "sweaters",
         "apparel"
-        ]
+        ],
+        type: "clothing",
+        sizeChartLink: "images/clothing-size-chart.png"
     },
     {
         id: "77919bbe-0e56-475b-adde-4f24dfed3a04",
@@ -708,6 +712,9 @@ export const products = [
         ]
     }
 ].map((productDetails) => {
+    if (productDetails.type === "clothing"){
+        return new clothing(productDetails);
+    }
     return new Product(productDetails);
 });
-console.log(products);
+

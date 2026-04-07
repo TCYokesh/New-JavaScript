c447fd5ef3174f01c8ab04fdd22b490325c4053e
Yokesh <yokeshyokesh1131.sse@saveetha.com>
2026-04-07 10:01:12 +0530

    added backend

--- Amazon/Scripts/amazon.js
+++ Amazon/Scripts/amazon.js
@@ -1,9 +1,98 @@
 import { cart, saveToStorage, updateCartQuantity, addToCart } from "../data/cart.js";
 import { deliveryOptions } from "../data/deliveryoptions.js";
-import {products} from '../data/products.js';
+import {products,loadProducts} from '../data/products.js';
  
+loadProducts(renderProductsGrid);
 updateCartQuantity();
 
+function renderProductsGrid() {
+  let productsHTML = ''
+
+products.forEach((product) => {
+    productsHTML += `
+    <div class="product-container">
+          <div class="product-image-container">
+            <img class="product-image"
+              src="${product.image}">
+          </div>
+
+          <div class="product-name limit-text-to-2-lines">
+            ${product.name}
+          </div>
+
+          <div class="product-rating-container">
+            <img class="product-rating-stars"
+            
+              src="${product.getStarsUrl()}">
+            <div class="product-rating-count link-primary">
+              ${product.rating.count}
+            </div>
+          </div>
+
+          <div class="product-price">
+            ₹${product.getPrice()}
+          </div>
+
+          <div class="product-quantity-container">
+            <select class="js-quantity-selector-${product.id}">
+              <option selected value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+              <option value="6">6</option>
+              <option value="7">7</option>
+              <option value="8">8</option>
+              <option value="9">9</option>
+              <option value="10">10</option>
+            </select>
+          </div>
+
+          ${product.extraInfoHTML()}
+
+          <div class="product-spacer"></div>
+
+          <div class="added-to-cart js-added-${product.id}">
+            <img src="images/icons/checkmark.png">
+            Added
+          </div>
+
+          <button class="add-to-cart-button button-primary js-add-to-cart"
+          data-product-id="${product.id}">
+            Add to Cart
+          </button>
+        </div>
+    `;
+});
+
+document.querySelector('.js-products-grid').innerHTML = productsHTML;
+updateCartQuantity();
+document.querySelectorAll('select').forEach((select) => {
+    select.value = "1";
+});
+document.querySelectorAll('.js-add-to-cart').forEach((button) => {
+    button.addEventListener('click', () => {
+        const productId = button.dataset.productId;
+
+        let productContainer = button.closest('.product-container');
+        const quantitySelector = productContainer.querySelector(`[class^="js-quantity-selector-"]`);
+        const quantity = Number(quantitySelector.value);
+        addToCart(productId,quantity);
+        productContainer = button.closest('.product-container');
+        const addedMessage = productContainer.querySelector('.added-to-cart');
+        if (addedMessage) {
+            addedMessage.style.opacity = '1';
+
+        setTimeout(() => {
+            addedMessage.style.opacity = '0';
+        }, 2000);
+        }
+    });
+});
+}
+
+
+/*
 let productsHTML = ''
 
 products.forEach((product) => {
@@ -61,29 +150,30 @@ products.forEach((product) => {
           </button>
         </div>
     `;
-  });
+});
 
-    document.querySelector('.js-products-grid').innerHTML = productsHTML;
-    updateCartQuantity();
-    document.querySelectorAll('select').forEach((select) => {
-      select.value = "1";
+document.querySelector('.js-products-grid').innerHTML = productsHTML;
+updateCartQuantity();
+document.querySelectorAll('select').forEach((select) => {
+    select.value = "1";
+});
+document.querySelectorAll('.js-add-to-cart').forEach((button) => {
+    button.addEventListener('click', () => {
+        const productId = button.dataset.productId;
+
+        let productContainer = button.closest('.product-container');
+        const quantitySelector = productContainer.querySelector(`[class^="js-quantity-selector-"]`);
+        const quantity = Number(quantitySelector.value);
+        addToCart(productId,quantity);
+        productContainer = button.closest('.product-container');
+        const addedMessage = productContainer.querySelector('.added-to-cart');
+        if (addedMessage) {
+            addedMessage.style.opacity = '1';
+
+        setTimeout(() => {
+            addedMessage.style.opacity = '0';
+        }, 2000);
+        }
     });
-    document.querySelectorAll('.js-add-to-cart').forEach((button) => {
-        button.addEventListener('click', () => {
-          const productId = button.dataset.productId;
-
-          let productContainer = button.closest('.product-container');
-          const quantitySelector = productContainer.querySelector(`[class^="js-quantity-selector-"]`);
-          const quantity = Number(quantitySelector.value);
-          addToCart(productId,quantity);
-          productContainer = button.closest('.product-container');
-          const addedMessage = productContainer.querySelector('.added-to-cart');
-          if (addedMessage) {
-              addedMessage.style.opacity = '1';
-
-            setTimeout(() => {
-              addedMessage.style.opacity = '0';
-            }, 2000);
-          }
-        });
-    });
+});
+*/
--- Amazon/data/backend-practice.js
+++ Amazon/data/backend-practice.js
@@ -6,3 +6,25 @@ xhr.addEventListener('load',() => {
 
 xhr.open('GET', 'https://supersimplebackend.dev');
 xhr.send();
+
+
+export let products = [];
+export function loadProducts(fun){
+    const xhr = new XMLHttpRequest();
+    
+    xhr.addEventListener('load', () => {
+        products = JSON.parse(xhr.response).map((productDetails) => {
+            if (productDetails.type === "clothing"){
+                return new clothing(productDetails);
+            }
+            else if (productDetails.type === "appliance"){
+                return new appliance(productDetails);
+            }
+            return new Product(productDetails);
+        });
+        console.log('load products');
+        fun();
+    });
+    xhr.open('GET','https://supersimplebackend.dev/products');
+    xhr.send();
+}
